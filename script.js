553214745b0e6ac725bd2f370c2f40f08c0f0ce1
IraM-12 <138891421+IraM-12@users.noreply.github.com>
2023-07-27 14:23:51 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,49 @@
+// задание 1
+var x, y, z;
+var x = 2;
+var y = 3;
+z = x ^ 3 + y ^ 3
+
+// задание 2
+var x;
+if (x) number = Int(number); {// call func} ;
+    if (
+        console.log("Значение задано неверно")
+    )
+        var result;
+    result = x - x / 100 * 13;
+    console.log("Размер заработной платы за вычетом налогов равен значение");
+    // задание 3
+    let a, b, c, message = 'Введите число';
+    
+    
+
+        if (a > b)
+            if (a > c) {
+                System.out.println("Наибольшее из трёх число" + " " + a);
+            }
+        if (b > c)
+            if (b > a) {
+                System.out.println("Наибольшее из трёх число" + " " + b);
+            }
+
+        if (c > a)
+            if (c > b) {
+                System.out.println("Наибольшее из трёх число" + " " + c);
+            }
+
+        // задание 4
+        var a = 6;
+        var b = 2;
+        function sum(a, b) {
+            console.log(a + b);
+        }
+        function diff(a, b) {
+            console.log(a - b);
+        }
+        function prod(a, b) {
+            console.log(a*b);
+        }
+        function division(a,b){
+            console.log(a/b);}
+        
